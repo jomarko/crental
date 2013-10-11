3593611494b0daf5f6ac8d1a5326b62259e63c03
patrik <patrik@tara>
2013-10-11 08:38:21 +0200

    DAOCarImpl source format

--- src/main/java/cz/muni/fi/pompe/crental/DAOCarImpl.java
+++ src/main/java/cz/muni/fi/pompe/crental/DAOCarImpl.java
@@ -6,121 +6,107 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-    
-
 /**
  *
  * @author Roman Stehlik
  */
-public class DAOCarImpl implements DAOCar{
+public class DAOCarImpl implements DAOCar {
+
     private EntityManagerFactory emf;
-    
+
     public void setEntityManagerFactory(EntityManagerFactory emf) {
         this.emf = emf;
     }
-   
-   
-     
-    
+
     @Override
     //Creates new car
-    public void createCar(Car car){
+    public void createCar(Car car) {
         EntityManager em = emf.createEntityManager();
-        try{
+        try {
             em.getTransaction().begin();
             em.persist(car);
             em.getTransaction().commit();
-        }finally{
+        } finally {
             em.close();
         }
-        }
+    }
 
     @Override
-    public void deleteCar(Car car){
+    public void deleteCar(Car car) {
         checkEntityManagerFactory();
-                
+
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             Car newCar = em.find(Car.class, car.getId());
 
             em.getTransaction().begin();
             em.remove(newCar);
             em.getTransaction().commit();
-        }finally{
+        } finally {
             em.close();
         }
-        }
+    }
 
     @Override
     public void updateCar(Car car) {
         checkEntityManagerFactory();
-              
+
         EntityManager em = emf.createEntityManager();
-        
-        try{
+
+        try {
             Car newCar = em.find(Car.class, car.getId());
 
             em.getTransaction().begin();
             newCar.setCarType(car.getCarType());
             newCar.setEvidencePlate(car.getEvidencePlate());
             em.getTransaction().commit();
-        }finally{
+        } finally {
             em.close();
         }
     }
-        
-       
 
     private void checkEntityManagerFactory() {
-        if(emf == null) {
+        if (emf == null) {
             throw new IllegalStateException("EntityManagerFactory emf was not set");
         }
     }
 
-    
-     
     @Override
     public List<Car> getAllCars() {
-        checkEntityManagerFactory();              
+        checkEntityManagerFactory();
         EntityManager em = emf.createEntityManager();
         try {
-                Query q = em.createNamedQuery("Car.SelectAllCars", Car.class);
-                List<Car> result = q.getResultList();
-                return result;
-            }
-        finally {
-                em.close();
-                }        
+            Query q = em.createNamedQuery("Car.SelectAllCars", Car.class);
+            List<Car> result = q.getResultList();
+            return result;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public Car getCarById(Long id) {
-        checkEntityManagerFactory();              
+        checkEntityManagerFactory();
         EntityManager em = emf.createEntityManager();
         try {
-                Query q = em.createNamedQuery("Car.SelectCarById", Car.class);
-                
-                return (Car)q.getSingleResult();
-            }
-        finally {
-                em.close();
-                }        
-    }
-    
-     public Car getCarByLicencePlate(String licencePlate) {
-        checkEntityManagerFactory();              
-        EntityManager em = emf.createEntityManager();
-        try {
-                Query q = em.createNamedQuery("Car.SelectCarByLicencePlate", Car.class);
-                
-                return (Car)q.getSingleResult();
-            }
-        finally {
-                em.close();
-                }        
-    }
-    
-      
+            Query q = em.createNamedQuery("Car.SelectCarById", Car.class);
 
+            return (Car) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Car getCarByLicencePlate(String licencePlate) {
+        checkEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query q = em.createNamedQuery("Car.SelectCarByLicencePlate", Car.class);
+
+            return (Car) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
 }
