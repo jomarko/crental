93299514645996373b09e6b49092301d915d09c6
Jozef Marko <jozefmarko1992@gmail.com>
2013-10-11 14:01:13 +0200

    DaoCar testy

--- src/test/java/cz/muni/fi/pompe/crental/DAOCarImplTest.java
+++ src/test/java/cz/muni/fi/pompe/crental/DAOCarImplTest.java
@@ -5,10 +5,12 @@
 package cz.muni.fi.pompe.crental;
 
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -32,26 +34,138 @@ public class DAOCarImplTest {
     
     @Test
     public void testCreateCar() {
+        Car car = newCar(null, "cartype", "spz");
+        daocar.createCar(car);
+        
+        Car result = daocar.getCarById(car.getId());
+        assertEquals(car.getId(), result.getId());
+        assertEquals(car.getCarType(), result.getCarType());
+        assertEquals(car.getEvidencePlate(), result.getEvidencePlate());
+               
+        try{
+            daocar.createCar(null);
+            fail("null car");
+        }catch(NullPointerException ex){    }
+        
+        try{
+            car = newCar(null, "", null);
+            daocar.createCar(car);
+            fail("invalid car");
+        }catch(IllegalArgumentException ex){    }
         
     }
     
     @Test
     public void testUpdateCar() {
+        Car car = newCar(null, "cartype", "spz");
+        daocar.createCar(car);
         
+        car.setEvidencePlate("spzka");
+        daocar.updateCar(car);
+        
+        Car result = daocar.getCarById(car.getId());
+        assertEquals(car.getId(), result.getId());
+        assertEquals(car.getCarType(), result.getCarType());
+        assertEquals(car.getEvidencePlate(), result.getEvidencePlate());
+               
+        try{
+            daocar.updateCar(null);
+            fail("null car");
+        }catch(NullPointerException ex){    }
+        
+        try{
+            car.setCarType("");
+            daocar.updateCar(car);
+            fail("invalid car");
+        }catch(IllegalArgumentException ex){    }
+        
+        try{
+            car = daocar.getCarById(car.getId());
+            car.setId(car.getId()+1);
+            daocar.updateCar(car);
+            fail("wrong id of car");
+        }catch(NullPointerException ex){    }
     }
     
     @Test
     public void testDeleteCar() {
+        Car car = newCar(null, "cartype", "spz");
+        Car auto = newCar(null, "autotype", "autospz");
+        daocar.createCar(car);
+        daocar.createCar(auto);
         
+        assertEquals(daocar.getAllCars().size(), 2);
+        
+        daocar.deleteCar(car);
+        
+        assertEquals(daocar.getAllCars().size(), 1);
+        
+        assertEquals(daocar.getAllCars().get(0), auto);
+
+        daocar.deleteCar(auto);
+        
+        assertEquals(daocar.getAllCars().size(), 0);
+        
+        try{
+            daocar.deleteCar(null);
+            fail("null car");
+        }catch(NullPointerException ex){    }
+        
+        auto = newCar(null, "typ", "spz");
+        daocar.createCar(auto);
+        car = newCar(11452l, "cartype", "spz");
+        try{
+            daocar.deleteCar(car);
+            fail("invalid car");
+        }catch(IllegalArgumentException ex){    }
     }
     
     @Test
-    public void testGetAllRequest() {
+    public void testGetAllCars() {
+        Car car = newCar(null, "cartype", "spz");
+        daocar.createCar(car);
+        Car auto = newCar(null, "autotype", "spz");
+        daocar.createCar(auto);
         
+        assertEquals(daocar.getAllCars().size(), 2);
+        assertEquals(daocar.getAllCars().get(0), car);
+        assertEquals(daocar.getAllCars().get(1), auto);
+               
+        daocar.deleteCar(car);
+        daocar.deleteCar(auto);
+        
+        assertEquals(daocar.getAllCars().size(), 0);
     }
     
     @Test
     public void testGetCarById() {
+        Car car = newCar(null, "cartype", "spz");
+        daocar.createCar(car);
+        Car auto = newCar(null, "autotype", "spz");
+        daocar.createCar(auto);
         
+        assertEquals(daocar.getCarById(car.getId()), car);
+        assertEquals(daocar.getCarById(auto.getId()), auto);
+        
+        try{
+            daocar.getCarById(null);
+        }catch(NullPointerException ex){
+            
+        }
+        
+        try{
+            daocar.getCarById(745l);
+        }catch(NoResultException ex){
+            
+        }
+    }
+    
+    private Car newCar(Long id, String carType, String evidencePlate){
+        Car c = new Car();
+        c.setId(id);
+        c.setCarType(carType);
+        c.setEvidencePlate(evidencePlate);
+        
+        return c;
     }
 }
