273ac27ef5961c3ff7e811b4222d0b07cf1229bc
Yayo <Yayo@Yayo-PC>
2013-10-27 21:42:15 +0100

    DAORentImplTest - pridane testy v kazdej CRUD metode, testovanie zlych atributov a null objektov predavanych do funkcie

--- src/test/java/cz/muni/fi/pompe/crental/dao/DAORentImplTest.java
+++ src/test/java/cz/muni/fi/pompe/crental/dao/DAORentImplTest.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pompe.crental.dao;
 
+import cz.fi.muni.pompe.crental.exception.CarRentalException;
 import cz.muni.fi.pompe.crental.entity.Employee;
 import cz.muni.fi.pompe.crental.entity.Car;
 import cz.muni.fi.pompe.crental.entity.AccessRight;
@@ -24,13 +25,13 @@ import org.junit.Test;
  * @author jozef
  */
 public class DAORentImplTest {
+
     private EntityManagerFactory emf;
     private DAORentImpl daoRent;
     private DAOCarImpl daoCar;
     private DAOEmployeeImpl daoEmployee;
     private DAORequestImpl daoRequest;
-    
-    
+
     @Before
     public void setUp() {
         emf = Persistence.createEntityManagerFactory("CarRentalPUInMemory");
@@ -39,141 +40,185 @@ public class DAORentImplTest {
         daoEmployee = new DAOEmployeeImpl(emf);
         daoCar = new DAOCarImpl(emf);
     }
-    
+
     @After
     public void tearDown() {
         emf.close();
     }
-    
+
     @Test
-    public void testCreateRent() {
+    public void testCreateRent() throws CarRentalException {
         Employee admin = DAOEmployeeImplTest.newEmployee(null, "Patrik Pomope", "XXXX", AccessRight.Admin);
         daoEmployee.createEmployee(admin);
-        
+
         Employee employee = DAOEmployeeImplTest.newEmployee(null, "Just an Employee", "XXXX", AccessRight.Employee);
         daoEmployee.createEmployee(employee);
         Rent rent = getSampleRent();
-        
-        rent.setConfirmedBy(employee);
-        
-        try {
-            daoRent.createRent(rent);
-            fail("cannt be confirmed by employee");
-        } catch (Exception e) { }
-        
-        rent.setConfirmedBy(admin);
-        
+
+        //test of creating valid instance of Rent
         int before = daoRent.getAllRents().size();
         daoRent.createRent(rent);
         assertEquals(before + 1, daoRent.getAllRents().size());
+
+        //tests of creating invalid instance of Rent
+        rent.setConfirmedBy(employee);
+        try {
+            daoRent.createRent(rent);
+            fail("Rent cannot be confirmed by employee");
+        } catch (Exception e) {
+        }
+
+        rent.setId(5l);
+        rent.setConfirmedBy(admin);
+        try {
+            daoRent.createRent(rent);
+            fail("cannot persist rent with setted id");
+        } catch (Exception e) {
+        }
+
+        rent.setId(null);
+        rent.setRentedCar(null);
+        rent.setConfirmedAt(null);
+        try {
+            daoRent.createRent(rent);
+            fail("cannot persist rent with wrong attributes");
+        } catch (Exception e) {
+        }
+
+        try {
+            daoRent.createRent(null);
+            fail("cannot persist when instance of Rent is null");
+        } catch (Exception e) {
+        }
+
     }
-    
+
     @Test
-    public void testUpdateRent() {
+    public void testUpdateRent() throws CarRentalException {
         Rent rent = getSampleRent();
         daoRent.createRent(rent);
-        
+
         Employee admin = DAOEmployeeImplTest.newEmployee(null, "Admin Adminuv", "XXX", AccessRight.Admin);
         daoEmployee.createEmployee(admin);
-        
+
         Car octavia = new Car();
         octavia.setCarType("Skoda octavia");
         octavia.setEvidencePlate("SPZ");
         daoCar.createCar(octavia);
-        
+
         Request request = new Request();
-        request.setDateFrom(new Date(113,9,15));
-        request.setDateTo(new Date(113,9,16));
+        request.setDateFrom(new Date(113, 9, 15));
+        request.setDateTo(new Date(113, 9, 16));
         request.setDescription("test");
         request.setEmployee(admin);
         daoRequest.createRequest(request);
-        
+
         rent.setConfirmedAt(new Date(113, 9, 12));
         rent.setConfirmedBy(admin);
         rent.setRentedCar(octavia);
         rent.setRequest(request);
         daoRent.updateRent(rent);
-        
+
         Rent updated = daoRent.getRentById(rent.getId());
-        
-        assertEquals(updated.getConfirmedAt(), rent.getConfirmedAt());
-        assertEquals(updated.getConfirmedBy(), rent.getConfirmedBy());
-        assertEquals(updated.getId(), rent.getId());
-        assertEquals(updated.getRentedCar(), rent.getRentedCar());
-        assertEquals(updated.getRequest().getId(), rent.getRequest().getId());
+
+        assertDeepEquals(rent, updated);
     }
-    
+
     @Test
-    public void testDeleteRent() {
-        int counBefore = this.daoRent.getAllRents().size();
+    public void testDeleteRent() throws CarRentalException {
+        int countBefore = this.daoRent.getAllRents().size();
         Rent rent = getSampleRent();
+
+        //test of deleting valid instance of Rent
         daoRent.createRent(rent);
-        daoRent.deleteRent(rent);
+        daoRent.deleteRent(rent.getId());
+        assertEquals(countBefore, daoRent.getAllRents().size());
+
+        //test of deleting invalid instance of Rent
+        rent.setId(null);
+        daoRent.createRent(rent);
         
-        assertEquals(counBefore, daoRent.getAllRents().size());
+        try {
+            daoRent.deleteRent(null);
+            fail("cannot delete when instance of Rent is null");
+        } catch (Exception e) {
+        }
+        
+        try {
+            daoRent.deleteRent(rent.getId() + 1);
+            fail("cannot delete Rent when given id is not in the database");
+        } catch (Exception e) {
+        }
+        
     }
-    
+
     @Test
-    public void testGetAllRequest() {
+    public void testGetAllRequest() throws CarRentalException {
         List<Rent> list = daoRent.getAllRents();
         Rent rent = getSampleRent();
         daoRent.createRent(rent);
         list.add(rent);
         List<Rent> expected = daoRent.getAllRents();
-        
+
         assertEquals(expected.size(), list.size());
     }
-    
+
     @Test
-    public void testGetRentById() {
+    public void testGetRentById() throws CarRentalException {
         Rent rent = getSampleRent();
         daoRent.createRent(rent);
-        
+
         Rent rent2 = daoRent.getRentById(rent.getId());
-        
-        assertEquals(rent2.getConfirmedAt().getTime(), rent.getConfirmedAt().getTime());
-        assertEquals(rent2.getConfirmedBy(), rent.getConfirmedBy());
-        assertEquals(rent2.getId(), rent.getId());
-        assertEquals(rent2.getRentedCar(), rent.getRentedCar());
-        assertEquals(rent2.getRequest().getId(), rent.getRequest().getId());
-        
-        try{
+
+        assertDeepEquals(rent, rent2);
+
+        try {
             daoRent.getRentById(Long.MAX_VALUE);
             fail("rent with MAX_VALUE_ID should not exist");
-        } catch (Exception ex) { }
-        
-        try{
+        } catch (Exception ex) {
+        }
+
+        try {
             daoRent.getRentById(null);
             fail("rent with nullable id found");
-        } catch (Exception ex) { }
+        } catch (Exception ex) {
+        }
     }
-    
+
     public Rent getSampleRent() {
         Employee admin = DAOEmployeeImplTest.newEmployee(null, "Patrik Pomope", "XXXX", AccessRight.Admin);
         daoEmployee.createEmployee(admin);
-        
+
         Employee employee = DAOEmployeeImplTest.newEmployee(null, "Just an Employee", "XXXX", AccessRight.Employee);
         daoEmployee.createEmployee(employee);
-        
+
         Car car = new Car();
         car.setCarType("Fabia");
         car.setEvidencePlate("SPZka");
         daoCar.createCar(car);
-        
+
         Request req = new Request();
-        req.setDateFrom(new Date(113,9,20));
-        req.setDateTo(new Date(113,9,20));
+        req.setDateFrom(new Date(113, 9, 20));
+        req.setDateTo(new Date(113, 9, 20));
         req.setDescription("Chci fabii");
         req.setEmployee(employee);
-        
+
         daoRequest.createRequest(req);
-        
+
         Rent rent = new Rent();
         rent.setRequest(req);
-        rent.setConfirmedAt(new Date(113,9,20));
+        rent.setConfirmedAt(new Date(113, 9, 20));
         rent.setRentedCar(car);
         rent.setConfirmedBy(admin);
-        
+
         return rent;
+    }
+
+    static void assertDeepEquals(Rent expected, Rent actual) {
+        assertEquals(expected.getConfirmedAt(), actual.getConfirmedAt());
+        assertEquals(expected.getConfirmedBy(), actual.getConfirmedBy());
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getRentedCar(), actual.getRentedCar());
+        assertEquals(expected.getRequest().getId(), actual.getRequest().getId());
     }
 }
