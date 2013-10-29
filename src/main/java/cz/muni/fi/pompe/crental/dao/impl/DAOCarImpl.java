769cefd0d5c254b0403afb3efcf11196233ba15e
patrik <patrik@tara>
2013-10-29 22:48:31 +0100

    DAOCar injektuje primo entityManagera a transakce tak resi az servisni vrstva

--- src/main/java/cz/muni/fi/pompe/crental/dao/impl/DAOCarImpl.java
+++ src/main/java/cz/muni/fi/pompe/crental/dao/impl/DAOCarImpl.java
@@ -6,9 +6,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 /**
@@ -16,30 +14,21 @@ import javax.persistence.Query;
  * @author Roman Stehlik
  */
 public class DAOCarImpl implements DAOCar {
-
-    private EntityManagerFactory emf;
+    @PersistenceContext
+    EntityManager em;
+    
     private static final Logger LOG = Logger.getLogger(DAOCarImpl.class.getName());
     
-    public DAOCarImpl(EntityManagerFactory emf) {
-        this.emf = emf;
+    public DAOCarImpl() {
+        
     }
-
-    @PersistenceUnit
-    public void setEmf(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
-    
-    
 
     @Override
-    //Creates new car
     public void createCar(Car car) {
-        EntityManager em = emf.createEntityManager();
         checkCar(car);
+        
         try {
-            em.getTransaction().begin();
             em.persist(car);
-            em.getTransaction().commit();
         } finally {
             em.close();
         }
@@ -49,14 +38,11 @@ public class DAOCarImpl implements DAOCar {
     @Override
     public void deleteCar(Car car) {
         checkCar(car);
-        EntityManager em = emf.createEntityManager();
 
         try {
             Car newCar = em.find(Car.class, car.getId());
 
-            em.getTransaction().begin();
             em.remove(newCar);
-            em.getTransaction().commit();
         } finally {
             em.close();
         }
@@ -66,15 +52,12 @@ public class DAOCarImpl implements DAOCar {
     @Override
     public void updateCar(Car car) {
         checkCar(car);
-        EntityManager em = emf.createEntityManager();
 
         try {
             Car newCar = em.find(Car.class, car.getId());
 
-            em.getTransaction().begin();
             newCar.setCarType(car.getCarType());
             newCar.setEvidencePlate(car.getEvidencePlate());
-            em.getTransaction().commit();
         } finally {
             em.close();
         }
@@ -83,7 +66,6 @@ public class DAOCarImpl implements DAOCar {
 
     @Override
     public List<Car> getAllCars() {
-        EntityManager em = emf.createEntityManager();
         try {
             Query q = em.createNamedQuery("Car.SelectAllCars", Car.class);
             List<Car> result = q.getResultList();
@@ -95,10 +77,10 @@ public class DAOCarImpl implements DAOCar {
 
     @Override
     public Car getCarById(Long id) {
-        if(id == null){
+        if (id == null){
             throw new NullPointerException("given id was null");
         }        
-        EntityManager em = emf.createEntityManager();
+
         try {
             Query q = em.createNamedQuery("Car.SelectCarById", Car.class);
             q.setParameter("id", id);
@@ -109,15 +91,15 @@ public class DAOCarImpl implements DAOCar {
     }
     
     private void checkCar(Car car){
-        if(car == null){
+        if (car == null){
             throw new NullPointerException("car was null");
         }
         
-        if(car.getCarType() == null || car.getCarType().isEmpty()){
+        if (car.getCarType() == null || car.getCarType().isEmpty()){
             throw new IllegalArgumentException("carType cannt be null or empty");
         }
         
-        if(car.getEvidencePlate()== null || car.getEvidencePlate().isEmpty()){
+        if (car.getEvidencePlate()== null || car.getEvidencePlate().isEmpty()){
             throw new IllegalArgumentException("evidencePlate cannt be null or empty");
         }
     }
