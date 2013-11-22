aed691bb3ff247c849d5a80ffc289d3c4e70aa1f
Patrik Pompe <patrik.pompe@gmail.com>
2013-11-22 15:52:11 +0100

    =index napoveda + calendare min dates

--- crental-web/src/main/webapp/assets/js/crental.js
+++ crental-web/src/main/webapp/assets/js/crental.js
@@ -1,7 +1,9 @@
 
 $(function() {
     var $datepicker = $( "input.datepicker" );
-    $datepicker.datepicker();     
+    $datepicker.datepicker({
+        minDate: +1
+    });     
     
     $('.btn-danger').on('click', function() {
        return confirm(cr.i18n('common.deleteConfirm')); 
