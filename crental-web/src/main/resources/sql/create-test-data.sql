9bd92629e3e4d17947fdd6c7f128fd2cd5b630f7
patrik <patrik@tara>
2014-01-02 22:08:27 +0100

    vyssi hodnoty idcek v initial sql dumpu - viz kolize s nove vkladanymi hodnotami

--- crental-web/src/main/resources/sql/create-test-data.sql
+++ crental-web/src/main/resources/sql/create-test-data.sql
@@ -1,7 +1,8 @@
-insert into PA165.Employee (accessRight, name, password, id) values ('Admin', 'Ana von Admin', 'heslo', 1);
-insert into PA165.Employee (accessRight, name, password, id) values ('Employee', 'Jan Novák', 'heslo', 2);
-insert into PA165.Employee (accessRight, name, password, id) values ('Employee', 'Tomáš Jedno', 'heslo', 3);
+insert into PA165.Employee (accessRight, name, password, id) values ('Admin', 'Admin', 'heslo', 104);
+insert into PA165.Employee (accessRight, name, password, id) values ('Admin', 'Ana von Admin', 'heslo', 100);
+insert into PA165.Employee (accessRight, name, password, id) values ('Employee', 'Jan Novák', 'heslo', 101);
+insert into PA165.Employee (accessRight, name, password, id) values ('Employee', 'Tomáš Jedno', 'heslo', 103);
 
-insert into PA165.Car(id, carType, evidencePlate) values(1,'Škoda Octavia 2','3AR 0000');
-insert into PA165.Car(id, carType, evidencePlate) values(2,'BMW X5','9B5 0000');
-insert into PA165.Car(id, carType, evidencePlate) values(3,'Volvo XC60','6C6 0000');
+insert into PA165.Car(id, carType, evidencePlate) values(100,'Škoda Octavia 2','3AR 0000');
+insert into PA165.Car(id, carType, evidencePlate) values(101,'BMW X5','9B5 0000');
+insert into PA165.Car(id, carType, evidencePlate) values(103,'Volvo XC60','6C6 0000');
