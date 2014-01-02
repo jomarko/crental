c23e896746540e4483f4dea1c96f08c46df34523
patrik <patrik@tara>
2014-01-02 21:21:06 +0100

    mockovany security subject pro testy

--- crental-backend/src/test/java/cz/muni/fi/pompe/crental/service/AbstractIntegrationTest.java
+++ crental-backend/src/test/java/cz/muni/fi/pompe/crental/service/AbstractIntegrationTest.java
@@ -1,31 +1,97 @@
 package cz.muni.fi.pompe.crental.service;
 
+import org.apache.shiro.UnavailableSecurityManagerException;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.support.SubjectThreadState;
+import org.apache.shiro.util.LifecycleUtils;
+import org.apache.shiro.util.ThreadState;
+import org.junit.AfterClass;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import static org.mockito.Mockito.*;
 
 
-
-//@RunWith(MockitoJUnitRunner.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 @TransactionConfiguration(defaultRollback = true)
 @Transactional
-public abstract class AbstractIntegrationTest {
-    public AbstractIntegrationTest() {
-        this.authenticate();
+public abstract class AbstractIntegrationTest {       
+    private static ThreadState subjectThreadState;
+    
+    @BeforeClass
+    public static void initSecurityManager() {
+        setSecurityManager(new DefaultSecurityManager(new ShiroRealm()));
+    }
+    
+    @AfterClass
+    public static void tearDownShiro() {
+        doClearSubject();
+        try {
+            SecurityManager securityManager = getSecurityManager();
+            LifecycleUtils.destroy(securityManager);
+        } catch (UnavailableSecurityManagerException e) {
+            //we don't care about this when cleaning up the test environment
+            //(for example, maybe the subclass is a unit test and it didn't
+        }
+        setSecurityManager(null);
+    }
+    
+    @Before
+    public void initSecuritySubject() {
+        Subject subjectUnderTest = mock(Subject.class);
+        when(subjectUnderTest.isAuthenticated()).thenReturn(true);
+        when(subjectUnderTest.hasRole("admin")).thenReturn(true);
+
+        setSubject(subjectUnderTest);
     }
 
-    private void authenticate() {
-        UsernamePasswordToken token = new UsernamePasswordToken("rest", "rest"); // admin role, ktera je vzdy v systemu, muze delat vse - vhodna pro service testy
-//        token.setRememberMe(true);
-        Subject currentUser = SecurityUtils.getSubject();
-        currentUser.login(token);
+
+    /**
+     * Allows subclasses to set the currently executing {@link Subject} instance.
+     *
+     * @param subject the Subject instance
+     */
+    protected void setSubject(Subject subject) {
+        clearSubject();
+        subjectThreadState = createThreadState(subject);
+        subjectThreadState.bind();
+    }
+
+    protected Subject getSubject() {
+        return SecurityUtils.getSubject();
+    }
+
+    protected ThreadState createThreadState(Subject subject) {
+        return new SubjectThreadState(subject);
+    }
+
+    /**
+     * Clears Shiro's thread state, ensuring the thread remains clean for future test execution.
+     */
+    protected void clearSubject() {
+        doClearSubject();
+    }
+
+    private static void doClearSubject() {
+        if (subjectThreadState != null) {
+            subjectThreadState.clear();
+            subjectThreadState = null;
+        }
+    }
+
+    protected static void setSecurityManager(SecurityManager securityManager) {
+        SecurityUtils.setSecurityManager(securityManager);
+    }
+
+    protected static SecurityManager getSecurityManager() {
+        return SecurityUtils.getSecurityManager();
     }
 }
